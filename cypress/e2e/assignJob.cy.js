906bc0bd5eb2c70607ad6fff56d2b70478f14f9f
Vivek <vivek.eivor@gmail.com>
2023-03-24 17:53:38 +0530

    assign job

---
+++ cypress/e2e/assignJob.cy.js
@@ -0,0 +1,29 @@
+import Login from '../pages/login'
+import CreateJobOrder from '../pages/createNewJobOrder'
+import CreateJob from '../pages/createJob';
+
+describe('Create a new job', () => {
+    Cypress.on('uncaught:exception', () => {
+      return false;
+    });
+    it('New job', () => {
+
+        cy.viewport(1400,850)  
+
+        const login = new Login()
+        const joborder = new CreateJobOrder()
+        const job = new CreateJob()
+
+        const uuid = () => Cypress._.random(0, 1e4)
+        const id = uuid()
+        const jobtitle = `Test ${id}`
+
+        login.visitUrl()
+        login.actionVerifySignInPage()
+        login.actionLoginAsAdmin(Cypress.env('username'),Cypress.env('password'))
+        login.actionVerifyLogInAsAdmin()
+        joborder.actionCreateNewJobOrder(jobtitle)
+        job.actionCreateNewJob()
+        job.actionAssignJob()
+    });
+});
--- cypress/pages/createJob.js
+++ cypress/pages/createJob.js
@@ -5,6 +5,12 @@ class CreateJob {
     positionCount= ".numeric-positions.poistionCount";
     plusIcon= "//img[@class='f-right']";
     verifyCreatedJob= "//span[text()='Successfully added job!']";
+    editBtn= "//img[@title='Edit Detail']";
+    anywhereCheckbox= "//label[@for='work-location-anywhere']";
+    inviteBtn= "//button[@id='open-providers-dialog']";
+    checkbox= "(//input[@name='provider-choice'])[1]";
+    assignBtn= "//span[text()='Assign']";
+    updateBtn= "//button[@id='cmdPost']";
   
     actionCreateNewJob(){
       cy.log('Action: User is able to create new job')
@@ -25,6 +31,27 @@ class CreateJob {
       .click()
       cy.xpath(this.verifyCreatedJob)
       .should('include.text','Successfully added job!')
+    }
+    actionAssignJob(){
+      cy.log('Action: Assigned the job')
+      cy.xpath(this.editBtn)
+      .should('be.visible')
+      .click()
+      cy.xpath(this.anywhereCheckbox)
+      .should('be.visible')
+      .click()
+      cy.xpath(this.inviteBtn).scrollIntoView()
+      .click()
+      .wait(5000)
+      cy.xpath(this.checkbox)
+      .click()
+      cy.xpath(this.assignBtn)
+      .should('be.visible')
+      .click()
+      cy.xpath(this.updateBtn)
+      .should('be.visible')
+      .click()
+
     }
   }
   export default CreateJob;
